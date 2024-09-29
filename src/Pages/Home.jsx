ef6b42350538727e1bb88cfda111cd4f04ae7e72
Athil9609 <adhiladhi516@gmail.com>
2024-09-29 13:59:45 +0530

    second

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -7,7 +7,7 @@ function Home() {
     <>
     <div className='container-fluid p-2' >
       <Add/>
-      <div className='border rounded-5 d-flex flex-wrap gap-2 border-primary p-md-5 p-2 shadow'>
+      <div className='border rounded-5 d-flex flex-wrap gap-2 border-primary p-md-5 p-5 shadow'>
         <Books/>
 
       </div>
